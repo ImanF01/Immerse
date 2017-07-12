c77cbe695e8ead9f67f6c4bc332053f744909ea8
ImanF01 <imanfahmy01@gmail.com>
2017-07-12 09:31:53 -0700

    finished 12; able to recognize logged in users

--- User.swift
+++ User.swift
@@ -8,11 +8,10 @@
 
 import Foundation
 import FirebaseDatabase.FIRDataSnapshot
+
 class User: NSObject {
-    var followerCount: Int?
-    var followingCount: Int?
-    var postCount: Int?
-    var isFollowed = false
+    let uid: String
+    let username: String
     private static var _current: User?
     static var current: User {
         guard let currentUser = _current else {
@@ -20,35 +19,19 @@ class User: NSObject {
         }
         return currentUser
     }
-    class func setCurrent(_ user: User, writeToUserDefaults: Bool = false) {
-        if writeToUserDefaults {
-            let data = NSKeyedArchiver.archivedData(withRootObject: user)
-            UserDefaults.standard.set(data, forKey: Constants.UserDefaults.currentUser)
-        }
-        _current = user
-    }
-    let uid: String
-    let username: String
-    
+
     init(uid: String, username: String) {
         self.uid = uid
         self.username = username
         super.init()
     }
+    
     init?(snapshot: DataSnapshot) {
         guard let dict = snapshot.value as? [String : Any],
-            let username = dict["username"] as? String,
-            let followerCount = dict["follower_count"] as? Int,
-            let followingCount = dict["following_count"] as? Int,
-            let postCount = dict["post_count"] as? Int
+            let username = dict["username"] as? String
             else { return nil }
-        
         self.uid = snapshot.key
         self.username = username
-        self.followerCount = followerCount
-        self.followingCount = followingCount
-        self.postCount = postCount
-        
         super.init()
     }
     required init?(coder aDecoder: NSCoder) {
@@ -61,7 +44,14 @@ class User: NSObject {
         
         super.init()
     }
-}
+    class func setCurrent(_ user: User, writeToUserDefaults: Bool = false) {
+        if writeToUserDefaults {
+            let data = NSKeyedArchiver.archivedData(withRootObject: user)
+            UserDefaults.standard.set(data, forKey: Constants.UserDefaults.currentUser)
+        }
+        
+        _current = user
+    }}
 extension User: NSCoding {
     func encode(with aCoder: NSCoder) {
         aCoder.encode(uid, forKey: Constants.UserDefaults.uid)
