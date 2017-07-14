e19b0748228959663053fb4766f624b3bd7cc24e
ImanF01 <imanfahmy01@gmail.com>
2017-07-14 16:52:19 -0700

    added note title and note modification time label

---
+++ ListDraftTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ListDraftTableViewCell.swift
+//  Immerse
+//
+//  Created by Iman F on 7/14/17.
+//  Copyright © 2017 Iman F (group project). All rights reserved.
+//
+
+import UIKit
+
+class ListDraftTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var noteTitleLabel: UILabel!
+    @IBOutlet weak var noteModificationTimeLabel: UILabel!
+
+}
--- ListDraftTableViewController.swift
+++ ListDraftTableViewController.swift
@@ -18,8 +18,13 @@ class ListDraftTableViewController: UITableViewController {
         return 10
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "listDraftTableViewCell", for: indexPath)
-        cell.textLabel?.text = "Yay - it's working!"
-                return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listDraftTableViewCell", for: indexPath) as! ListDraftTableViewCell
+        cell.noteTitleLabel.text = "note's title"
+        cell.noteModificationTimeLabel.text = "note's modification time"
+        
+        return cell
     }
 }
+
+
+
