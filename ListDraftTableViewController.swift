5072fe5cc10149b985a885d8213acdf8cef65094
ImanF01 <imanfahmy01@gmail.com>
2017-07-16 14:16:29 -0700

    fixed table view cell

--- ListDraftTableViewController.swift
+++ ListDraftTableViewController.swift
@@ -2,7 +2,7 @@
 //  ListDraftTableViewController.swift
 //  Immerse
 //
-//  Created by Iman F on 7/14/17.
+//  Created by Iman F on 7/16/17.
 //  Copyright © 2017 Iman F (group project). All rights reserved.
 //
 
@@ -10,21 +10,23 @@ import UIKit
 import Hero
 
 class ListDraftTableViewController: UITableViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
+
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "listDraftTableViewCell", for: indexPath) as! ListDraftTableViewCell
-        cell.noteTitleLabel.text = "note's title"
-        cell.noteModificationTimeLabel.text = "note's modification time"
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listDraftTableViewCell", for: indexPath)
+        cell.textLabel?.text = "Yay! It's working"
         return cell
     }
+    
+
+    
 }
-
-
-
