9ac5a7ccd9d8dac0c828b225f21544ec7f1c3b76
ImanF01 <imanfahmy01@gmail.com>
2017-08-07 14:52:23 -0700

    fixed upload time

--- ProfileViewController.swift
+++ ProfileViewController.swift
@@ -57,8 +57,8 @@ class ProfileViewController: UIViewController {
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-            }
+        
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
--- StorageService.swift
+++ StorageService.swift
@@ -12,7 +12,7 @@ import UIKit
 
 struct StorageService {
     static func uploadImage(_ image: UIImage, at reference: StorageReference, completion: @escaping (URL?) -> Void) {
-        guard let imageData = UIImageJPEGRepresentation(image,0.1) else {
+        guard let imageData = UIImageJPEGRepresentation(image,0.05) else {
             return completion(nil)
         }
         reference.putData(imageData,metadata: nil, completion: { (metadata, error) in
